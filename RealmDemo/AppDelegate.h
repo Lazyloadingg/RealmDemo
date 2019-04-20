d4ebb33cb24557fa727ce68859cd4298b9b14910
lazyloading <lazyloading@163.com>
2019-04-20 16:37:38 +0800

    Initial Commit

---
+++ RealmDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealmDemo
+//
+//  Created by Apple on 2019/4/20.
+//  Copyright © 2019 lazyloading. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealmDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RealmDemo
+//
+//  Created by Apple on 2019/4/20.
+//  Copyright © 2019 lazyloading. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
